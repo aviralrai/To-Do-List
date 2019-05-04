9d6646058d62cde8d432ec55ad4a94867f1ea5af
aviralrai <araviral33@gmail.com>
2019-05-04 10:12:48 +0530

    add a new list item

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,11 +1,22 @@
 //line-through and gray
-$("li").click(function(){
+$("ul").on("click","li",function(){
 	$(this).toggleClass("complete");
 });
+
 //functionality of delete
-$("span").click(function(event){
+$("ul").on("click","span",function(event){
 	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
 })
+
+//adding a new li
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13)
+	{
+		var text=$(this).val();
+		$(this).val("");
+		$("ul").append("<li><span>X</span> " + text +"</li>");
+	}
+});
